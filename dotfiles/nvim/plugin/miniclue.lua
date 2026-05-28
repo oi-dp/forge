a063d032f9d80b4ffd71e581d853a7b08ca56ea0
oi-dp <oi.dp@pm.me>
2026-05-28 15:55:48 +0700

    clean up

--- dotfiles/nvim/plugin/miniclue.lua
+++ dotfiles/nvim/plugin/miniclue.lua
@@ -27,16 +27,11 @@ mini_clue.setup {
         { mode = { 'n', 'x' }, keys = 'z' },
         { mode = { 'n', 'x' }, keys = '<Leader>ao', desc = '+OpenCode' },
         { mode = 'n', keys = '<Leader>b', desc = '+Buffer' },
-        { mode = 'n', keys = '<Leader>bs', desc = '+Scratch' },
         { mode = 'n', keys = '<Leader>e', desc = '+Explore' },
         { mode = 'n', keys = '<Leader>g', desc = '+Git' },
         { mode = 'n', keys = '<Leader>gb', desc = '+Blame' },
         { mode = 'n', keys = '<Leader>d', desc = '+Debug' },
         { mode = 'n', keys = '<Leader>l', desc = '+LSP' },
-        { mode = 'n', keys = '<Leader>t', desc = '+Testing' },
-        { mode = 'n', keys = '<Leader>tc', desc = '+Coverage' },
-        { mode = 'n', keys = '<Leader>o', desc = '+Other' },
-        { mode = 'n', keys = '<Leader>ox', desc = '+Quickfix' },
     },
     window = {
         config = {
